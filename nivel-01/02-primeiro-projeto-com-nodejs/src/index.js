eae27b51cb45532a0aab05fe385ce38f4f421a39
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-06-21 21:57:44 -0300

    Listando extrato

--- nivel-01/02-primeiro-projeto-com-nodejs/src/index.js
+++ nivel-01/02-primeiro-projeto-com-nodejs/src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -19,7 +20,7 @@ app.post("/account", (request, response) => {
       error: "Customer already exists!"
     });
   }
-  
+
   customers.push({
     cpf,
     name,
@@ -28,6 +29,14 @@ app.post("/account", (request, response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
