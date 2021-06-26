f93e65a81a7d4987136729e7b554240732ac6c85
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-06-25 23:21:41 -0300

    Atualizar conta

--- nivel-01/02-primeiro-projeto-com-nodejs/src/index.js
+++ nivel-01/02-primeiro-projeto-com-nodejs/src/index.js
@@ -114,4 +114,19 @@ app.get('/statement/date', verifyIfExistisAccountCPF, (request, response) => {
   return response.json(statement);
 });
 
+app.put('/account', verifyIfExistisAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get('/account',verifyIfExistisAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
 app.listen(3333);
