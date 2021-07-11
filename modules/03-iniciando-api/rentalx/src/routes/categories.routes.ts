aa32b9b980a9f3a58d33b1464113237c098e9896
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-07-10 21:56:27 -0300

    Nodejs: Listando as categorias

--- modules/03-iniciando-api/rentalx/src/routes/categories.routes.ts
+++ modules/03-iniciando-api/rentalx/src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post('/', (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get('/', (request, response) => {
+  const listCategories = categoriesRepository.list();
+
+  return response.json(listCategories);
+});
+
 export { categoriesRoutes };
