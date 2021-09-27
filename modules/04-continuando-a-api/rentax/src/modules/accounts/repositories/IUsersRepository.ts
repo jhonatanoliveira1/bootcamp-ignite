db8d5b45e2afac1e890156dfd53ccfb808a13429
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-09-27 07:48:26 -0300

    Nodejs: Criptografrar senha

--- modules/04-continuando-a-api/rentax/src/modules/accounts/repositories/IUsersRepository.ts
+++ modules/04-continuando-a-api/rentax/src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,4 +1,5 @@
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import User from '../entities/User';
 
 interface IUsersRepository {
   create({
@@ -7,6 +8,7 @@ interface IUsersRepository {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export default IUsersRepository;
