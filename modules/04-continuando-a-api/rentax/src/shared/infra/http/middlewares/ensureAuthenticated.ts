d6b8ab1348df3c562111bfaeb25c7b9463bcd027
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-10-15 14:23:51 -0300

    Nodejs: Criando middleware de administrador

--- modules/04-continuando-a-api/rentax/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ modules/04-continuando-a-api/rentax/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -13,35 +12,26 @@ export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction,
-) {
+): Promise<void> {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
 
-  const [, token] = authHeader.split('');
-
+  const [, token] = authHeader.split(' ');
   try {
     const { sub: user_id } = verify(
       token,
       '132a47bde754edb9e639a3e89d1b7d81',
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = await usersRepository.findByEmail(user_id);
-
-    if (!user) {
-      throw new AppError('User does not exists!', 401);
-    }
-
     request.user = {
       id: user_id,
     };
 
     next();
-  } catch {
+  } catch (error) {
     throw new AppError('Invalid token!', 401);
   }
 }
--- modules/04-continuando-a-api/rentax/src/shared/infra/http/routes/categories.routes.ts
+++ modules/04-continuando-a-api/rentax/src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer';
 import CreateCategoryController from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import ImportCategoryController from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import ListCategoryController from '@modules/cars/useCases/listCategories/ListCategoryController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const categoriesRoutes = Router();
 
@@ -15,13 +17,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoryController();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle,
+);
 
 categoriesRoutes.get('/', listCategoryController.handle);
 
 categoriesRoutes.post(
   '/import',
   upload.single('file'),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle,
 );
 
