e5a0477110a6c5de067674f0c3cbf41d9d0bb308
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2021-11-10 06:07:16 -0300

    Nodejs: Continuação CreateCarSpecificationUseCase

--- modules/04-continuando-a-api/rentax/src/modules/cars/repositories/ISpecificationsRepository.ts
+++ modules/04-continuando-a-api/rentax/src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -4,6 +4,7 @@ import Specification from '@modules/cars/infra/typeorm/entities/Specification';
 interface ISpecificationsRepository {
   create({ name, description }: ICreateSpecificationDTO): Promise<void>;
   findByName(name: string): Promise<Specification>;
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export default ISpecificationsRepository;
--- modules/04-continuando-a-api/rentax/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ modules/04-continuando-a-api/rentax/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,6 +1,7 @@
 import { inject } from 'tsyringe';
 
 import ICarsRepository from '@modules/cars/repositories/ICarsRepository';
+import ISpecificationsRepository from '@modules/cars/repositories/ISpecificationsRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -10,7 +11,11 @@ interface IRequest {
 
 class CreateCarSpecificationUseCase {
   // @inject('CarsRepository')
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    private carsRepository: ICarsRepository,
+
+    private specificationsRepository: ISpecificationsRepository,
+  ) {}
 
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
     const carExist = await this.carsRepository.findById(car_id);
@@ -18,6 +23,14 @@ class CreateCarSpecificationUseCase {
     if (!carExist) {
       throw new AppError('Car does not exists!');
     }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id,
+    );
+
+    carExist.specifications = specifications;
+
+    await this.carsRepository.create(carExist);
   }
 }
 
