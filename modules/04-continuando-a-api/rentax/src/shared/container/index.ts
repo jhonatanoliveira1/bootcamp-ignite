6df7cd923e6b79519dfe105bc18c2d49da389d8c
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2022-01-19 19:08:33 -0300

    Nodejs: Cadastro de imagens do carro

--- modules/04-continuando-a-api/rentax/src/shared/container/index.ts
+++ modules/04-continuando-a-api/rentax/src/shared/container/index.ts
@@ -4,7 +4,9 @@ import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersR
 import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
 import CategoriesRepository from '@modules/cars//infra/typeorm/repositories/CategoriesRepository';
 import SpecificationsRepository from '@modules/cars//infra/typeorm/repositories/SpecificationsRepository';
+import CarsImagesRepository from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
+import ICarsImagesRepository from '@modules/cars/repositories/ICarsImagesRepository';
 import ICarsRepository from '@modules/cars/repositories/ICarsRepository';
 import ICategoriesRepository from '@modules/cars/repositories/ICategoriesRepository';
 import ISpecificationsRepository from '@modules/cars/repositories/ISpecificationsRepository';
@@ -25,3 +27,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  'CarsImagesRepository',
+  CarsImagesRepository,
+);
