56f90b76f145c1f665fd3e5fe4f9acef29f85bb0
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2022-02-14 21:25:13 -0300

    Nodejs: Refatorando Autenticação do usuário

---
+++ modules/04-continuando-a-api/rentax/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '132a47bde754edb9e639a3e89d1b7d81',
+  expires_in_token: '15m',
+  secret_refresh_token: 'a25f052a84ffbc40e5eb98a609179b51',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
--- modules/04-continuando-a-api/rentax/src/shared/container/index.ts
+++ modules/04-continuando-a-api/rentax/src/shared/container/index.ts
@@ -3,7 +3,9 @@ import { container } from 'tsyringe';
 import '@shared/container/providers';
 
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import UsersTokensRepository from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
+import IUsersTokensRepository from '@modules/accounts/repositories/IUsersTokensRepository';
 import CategoriesRepository from '@modules/cars//infra/typeorm/repositories/CategoriesRepository';
 import SpecificationsRepository from '@modules/cars//infra/typeorm/repositories/SpecificationsRepository';
 import CarsImagesRepository from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
@@ -40,4 +42,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   'RentalsRepository',
   RentalsRepository,
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  'UsersTokensRepository',
+  UsersTokensRepository,
 );
