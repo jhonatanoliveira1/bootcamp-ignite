f42573ed4aeb12e010b8d15ad15f2a3b44a2fa4e
Jhonatan de Oliveira <jhonatan.d.oliveira1@outlook.com>
2022-02-16 22:20:13 -0300

    Nodejs: Controller refresh token

--- modules/04-continuando-a-api/rentax/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ modules/04-continuando-a-api/rentax/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import auth from '@config/auth';
+import UsersTokensRepository from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -14,17 +16,28 @@ export async function ensureAuthenticated(
   next: NextFunction,
 ): Promise<void> {
   const authHeader = request.headers.authorization;
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
+
   try {
     const { sub: user_id } = verify(
       token,
-      '132a47bde754edb9e639a3e89d1b7d81',
+      auth.secret_refresh_token,
     ) as IPayload;
+
+    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token,
+    );
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
 
     request.user = {
       id: user_id,
